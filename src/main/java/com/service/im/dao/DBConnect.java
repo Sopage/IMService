fd10acf3048168905c71b175aadfec039df07cb0
Mr.Huang <1123033157@qq.com>
2022-03-04 10:05:40 +0800

    update

--- src/main/java/com/service/im/dao/DBConnect.java
+++ src/main/java/com/service/im/dao/DBConnect.java
@@ -1,6 +1,7 @@
 package com.service.im.dao;
 
-import com.alibaba.druid.pool.DruidDataSourceFactory;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,6 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.Properties;
 
 public class DBConnect {
     private static final Logger LOGGER = LoggerFactory.getLogger(DBConnect.class);
@@ -16,9 +16,7 @@ public class DBConnect {
 
     public DBConnect() {
         try {
-            Properties properties = new Properties();
-            properties.load(getClass().getClassLoader().getResourceAsStream("druid.properties"));
-            dataSource = DruidDataSourceFactory.createDataSource(properties);
+            dataSource = new HikariDataSource(new HikariConfig());
         } catch (Exception e) {
             e.printStackTrace();
             LOGGER.error("数据库链接异常!!!", e);
@@ -32,7 +30,7 @@ public class DBConnect {
             ps.setObject(1, uid);
             ps.setObject(2, token);
             ResultSet rs = ps.executeQuery();
-            while (rs.next()){
+            while (rs.next()) {
                 uid = rs.getLong("id");
                 token = rs.getString("token");
                 LOGGER.info("uid={} token={}", uid, token);
--- src/main/java/com/service/im/redis/Redis.java
+++ src/main/java/com/service/im/redis/Redis.java
@@ -1,15 +1,17 @@
 package com.service.im.redis;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.async.RedisAsyncCommands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import redis.clients.jedis.Jedis;
 
 import java.nio.ByteBuffer;
 import java.util.List;
 
 public class Redis {
     private static final Logger LOGGER = LoggerFactory.getLogger(Redis.class);
-    private static Jedis jedis;
+    private static RedisAsyncCommands<String, String> redis;
 
     private Redis() {
 
@@ -17,25 +19,30 @@ public class Redis {
 
     public static void doInit() {
         try {
-            jedis = new Jedis("127.0.0.1", 6379);
-            jedis.connect();
-            if (jedis.isConnected()) {
+            StatefulRedisConnection<String, String> connection = RedisClient.create("redis://password@127.0.0.1:6379/0").connect();
+            if (connection.isOpen()) {
+                redis = connection.async();
                 LOGGER.info("redis 链接成功!!");
             } else {
                 LOGGER.error("redis 链接失败!!");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             LOGGER.error("redis 链接失败!!", e);
         }
 
     }
 
-    public static void putMessage(long uid, byte[] body) {
-        jedis.rpush(getLongToBytes(uid), body);
+    public static void putMessage(long uid, String body) {
+        redis.rpush(String.valueOf(uid), body);
     }
 
-    public static List<byte[]> getMessage(long uid) {
-        return jedis.lrange(getLongToBytes(uid), 0, -1);
+    public static List<String> getMessage(long uid) {
+        try {
+            return redis.lrange(String.valueOf(uid), 0, -1).get();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private static byte[] getLongToBytes(long l) {
